299ef7849829c6d5cad2c29b90658818f678c426
infinifier <kartiksajwan_dev@gmail.com>
2019-07-06 14:59:48 +0530

    Create, Edit, Delete POSTS
    Added functionality for adding, editing and deleting posts when a user is logged in.

--- src/articles/views.py
+++ src/articles/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from .models import Article
 # Create your views here.
@@ -8,3 +10,17 @@ from .models import Article
 class ArticleListView(ListView):
     model = Article
     template_name = 'article_list.html'
+
+class ArticleDetailView(DetailView):
+    model = Article
+    fields = ('title', 'body')
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    fields = ('title', 'body')
+    template_name = 'article_edit.html'
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list')
