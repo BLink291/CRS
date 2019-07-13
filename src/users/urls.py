31a54dae949765d329ed843285d7af65996cfc71
infinifier <kartiksajwan.dev.gmail.com>
2019-07-13 13:53:31 +0530

    cleanup

--- src/users/urls.py
+++ src/users/urls.py
@@ -3,6 +3,6 @@ from .views import SignUpView
 from django.urls import path
 
 urlpatterns = [
-    #path('login/', SignUpView.as_view(), name='login'),
+    
     path('signup/', SignUpView.as_view(), name='signup'),
 ]
